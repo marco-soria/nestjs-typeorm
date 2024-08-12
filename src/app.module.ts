288da53872d3325d75db77a27a56f6edd9719e95
marco-soria <marco.soriaa7@gmail.com>
2024-08-12 17:15:01 -0500

    injectable as value correction in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
-import { Client } from 'pg';
+
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -12,19 +13,7 @@ import { DatabaseModule } from './database/database.module';
 import { environments } from './environments';
 import config from './config';
 
-const client = new Client({
-  user: 'root',
-  host: 'localhost',
-  database: 'my_db',
-  password: '123456',
-  port: 5433,
-});
 
-client.connect();
-client.query('SELECT * FROM tasks', (err, res) => {
-  console.error(err);
-  console.log(res.rows);
-});
 
 @Module({
   imports: [
@@ -49,9 +38,8 @@ client.query('SELECT * FROM tasks', (err, res) => {
     {
       provide: 'TASKS',
       useFactory: async (http: HttpService) => {
-        const tasks = await http
-          .get('https://jsonplaceholder.typicode.com/todos')
-          .toPromise();
+        const request = http.get('https://jsonplaceholder.typicode.com/todos');
+        const tasks = await lastValueFrom(request);
         return tasks.data;
       },
       inject: [HttpService],
