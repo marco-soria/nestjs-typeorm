8dac74246cc4142a7a54c6bbc95e7779f14b3745
marco-soria <marco.soriaa7@gmail.com>
2024-08-18 23:20:53 -0500

    advance in scripts

---
+++ src/database/data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT, 10),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  logging: true,
+  entities: ['dist/**/*.entity.js'], // Asegúrate de que las entidades compiladas estén incluidas
+  synchronize: false,
+  migrations: ['dist/database/migrations/*.js'], // Asegúrate de que las migraciones compiladas estén incluidas
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
